f403cde898593d24d939f52b4c0e38bbf4b203aa
Abhay-Lejith <abhaylejith@gmail.com>
2023-05-11 18:00:31 +0530

    Added functionality to re-enter details if user/pass is wrong and login fails

--- login.sh
+++ login.sh
@@ -1,24 +1,35 @@
-
-if [ ! -f "login_details.txt" ];then
-    echo "Enter username:"
-    read user
-    echo "Enter password:"
-    read pass
-    echo $user >> login_details.txt
-    echo $pass >> login_details.txt
-fi
- 
-user=$(sed '1q;d' login_details.txt)
-pass=$(sed '2q;d' login_details.txt)
-
-nmcli radio wifi on
-
-until [ "$(nmcli -g connectivity general status)" == "portal" -o "$(nmcli -g connectivity general status)" == "full" ];
+status=1
+while [ $status == 1 ];
 do
-    sleep 2s
-done
+    if [ ! -f "login_details.txt" ];then             
+        echo "Enter username:"
+        read user
+        echo "Enter password:"
+        read pass
+        echo $user > login_details.txt
+        echo $pass >> login_details.txt
+    fi
+     
+    user=$(sed '1q;d' login_details.txt) 
+    pass=$(sed '2q;d' login_details.txt)
 
-curl -d "mode=191&username=${user}&password=${pass}&a=1683813525441&producttype=0" -X POST https://nac.nitk.ac.in:8090/httpclient.html -s 1> /dev/null
+    nmcli radio wifi on
+
+    until [ "$(nmcli -g connectivity general status)" == "portal" -o "$(nmcli -g connectivity general status)" == "full" ];
+    do
+        sleep 2s
+    done
+
+    curl -d "mode=191&username=${user}&password=${pass}&a=1683813525441&producttype=0" -X POST https://nac.nitk.ac.in:8090/httpclient.html -s > status.txt
+
+    grep -q "Invalid" status.txt
+    if [ $? == 0 ];then
+        echo "Invalid username/password. Enter again."
+        rm login_details.txt
+    else
+        status=0
+    fi
+done
 
 
 
